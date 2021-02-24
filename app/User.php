d28e2b2f792379c4073382714d805364ebba7311
Hazem <hazem@eramint.net>
2021-02-24 13:19:54 +0200

    msg

--- app/User.php
+++ app/User.php
@@ -111,9 +111,10 @@ class User extends Authenticatable
 
     public function getAvatarAttribute()
     {
-        return Storage::disk('uploads')
-                ->url('avatars/' . $this->id .
-                    '/avatar.png') . '?t=' . time();
+        // return Storage::disk('uploads')
+        //         ->url('avatars/' . $this->id .
+        //             '/avatar.png') . '?t=' . time();
+        return \URL::to('/').'/storage/app/public/avatars/'.$this->id. '/avatar.png' . '?t=' . time();
     }
 
     public function getTotalOrdersAttribute()
