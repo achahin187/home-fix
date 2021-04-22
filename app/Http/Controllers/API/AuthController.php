0f7280192d82bbb76ca983c154d1700f1e30c8be
Abdelrahman Shahin <a.shahin@eramint.net>
2021-04-22 16:17:35 +0200

    country info

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -53,8 +53,7 @@ class AuthController extends Controller
              if ($request->isMethod('get')) {
 
 
-                return __success(Country::where('status', true)
-                ->with('cities')->where('status', true)->get(), 200);
+                return __success(Country::where('status', true)->select('name_ar as name')->get(), 200);
 
 
 
