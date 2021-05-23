682953c8e4e22578dc50cda83b4c0c24ff1fc8f4
Abdelrahman Shahin <a.shahin@eramint.net>
2021-05-23 16:52:17 +0200

    offer

--- app/Category.php
+++ app/Category.php
@@ -33,7 +33,7 @@ class Category extends Model
 
     public function offers()
     {
-        dd(auth()->user()->name);
+        dd(auth()->user()->country_id);
 
             return $this->hasMany(Offer::class, 'category_id')->where('status', true)->where('country_id',auth()->user()->user_country_id);
 
