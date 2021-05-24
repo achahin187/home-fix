bc41ffe8c09a1c45b71622b6af48497d3c5783d5
Abdelrahman Shahin <a.shahin@eramint.net>
2021-05-24 11:16:15 +0200

    notifications

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -384,7 +384,8 @@ class ChatController extends Controller
 
     public function pushNotification_chat(Request $request)
     {
- dd($request->user['id']);
+
+ dd($request->id);
 
           $lang = User::where('id',$request->id)->first();
             $language = $lang->language;
